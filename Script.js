a438c7295b4f64fe11ad3294504a1488a19e5ce8
BartMid <bmiddelberg1@hotmail.com>
2017-09-15 12:06:00 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,17 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var ten = 10;
+var eleven = 11;
+var twelve = 12;
+var thirteen = 13;
+var fourteen = 14;
+var fifteen = 15;
+var sixteen = true;
+var seventeen = "Kevin de Staat";
